a4e875cc0c827472e51461bb149fc47e7f0ae5c8
Phoebe Jackson <pcaj2@cam.ac.uk>
2021-06-23 13:53:09 +0100

    Calculator with if

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const readline = require('readline-sync');
 console.log("Welcome to my calculator!");
 
+console.log('Please enter an operator:');
+const operator = readline.prompt();
 console.log('Please enter a number:');
-const number1 = readline.prompt();
+const number1 = +readline.prompt();
 console.log('Please enter another number:');
-const number2 = readline.prompt();
-console.log(number1 + " multiplied by " + number2 + " is " + number1*number2)
+const number2 = +readline.prompt();
+
+if (operator == "+"){
+    console.log(number1 + " " + operator + " " + number2 + " is " + ( number1 + number2 ) )
+} else if (operator == "*"){
+    console.log(number1 + " " + operator + " " + number2 + " is " + ( number1 * number2 ) )
+} else if (operator == "-"){
+    console.log(number1 + " " + operator + " " + number2 + " is " + ( number1 - number2 ) )
+} else if (operator == "/"){
+    console.log(number1 + " " + operator + " " + number2 + " is " + ( number1 / number2 ) )
+} else {
+    console.log("Invalid operator")
+}
