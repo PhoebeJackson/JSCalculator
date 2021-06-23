588cf2f1948cccf371af5c21fc70cf4fd35b137f
Phoebe Jackson <pcaj2@cam.ac.uk>
2021-06-23 11:59:54 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const readline = require('readline-sync');
 console.log("Welcome to my calculator!");
+
+console.log('Please enter a number:');
+const number1 = readline.prompt();
+console.log('Please enter another number:');
+const number2 = readline.prompt();
+console.log(number1 + " multiplied by " + number2 + " is " + number1*number2)
