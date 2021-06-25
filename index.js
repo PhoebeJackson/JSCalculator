9ba7704f73672935bc702de7d47aa142e24cf5ed
Phoebe Jackson <pcaj2@cam.ac.uk>
2021-06-25 18:57:10 +0100

    Multiple numbers

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ console.log("Welcome to my calculator!");
 
 console.log('Please enter an operator:');
 const operator = readline.prompt();
-console.log('Please enter a number:');
-const number1 = +readline.prompt();
-console.log('Please enter another number:');
-const number2 = +readline.prompt();
+console.log('How many numbers would you like to ' + operator + ':');
+const quantity = +readline.prompt();
 
+let numbers = [];
+for (let i = 0; i < quantity; i++) {
+    console.log('Please enter another number:');
+    numbers.push(+readline.prompt());
+}
+let answer
+let string = ""
 switch (operator){
     case "+":
-        console.log(number1 + " " + operator + " " + number2 + " is " + ( number1 + number2 ) );
+        answer = 0;
+        numbers.forEach(function(element, index) {if (index == 0){string += element;} else {string += " + " + element}; answer+=element;})
         break;
     case "*":
-        console.log(number1 + " " + operator + " " + number2 + " is " + ( number1 * number2 ) );
+        answer = 1;
+        numbers.forEach(function(element, index) {if (index == 0){string += element;} else {string += " * " + element}; answer*=element;})
         break;
     case "-":
-        console.log(number1 + " " + operator + " " + number2 + " is " + ( number1 - number2 ) );
+        numbers.forEach(function(element, index) {if (index == 0){string += element; answer = element;} else {string += " - " + element; answer-=element;}})
         break;
     case "/":
-        console.log(number1 + " " + operator + " " + number2 + " is " + ( number1 / number2 ) );
+        numbers.forEach(function(element, index) {if (index == 0){string += element; answer = element} else {string += " / " + element; answer/=element;}})
         break;
     default:
         console.log("Invalid operator");
 }
+console.log(string + " is " + answer );
