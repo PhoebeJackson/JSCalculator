440bbfddd7aa95b9bb193dbf535be3f8e47abaf4
Phoebe Jackson <pcaj2@cam.ac.uk>
2021-06-26 10:24:43 +0100

    while loops and functions

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
 const readline = require('readline-sync');
-console.log("Welcome to my calculator!");
 
-console.log('Please enter an operator:');
-const operator = readline.prompt();
-console.log('How many numbers would you like to ' + operator + ':');
-const quantity = +readline.prompt();
+function printWelcomeMessage(){
+    console.log("Welcome to my calculator!");
+}
 
-let numbers = [];
-for (let i = 0; i < quantity; i++) {
-    console.log('Please enter another number:');
-    numbers.push(+readline.prompt());
+function goAgain(){
+    console.log('Another Calculation? (yes)');
+    return readline.prompt() == 'yes';
 }
-let answer
-let string = ""
-switch (operator){
-    case "+":
-        answer = 0;
-        numbers.forEach(function(element, index) {if (index == 0){string += element;} else {string += " + " + element}; answer+=element;})
-        break;
-    case "*":
-        answer = 1;
-        numbers.forEach(function(element, index) {if (index == 0){string += element;} else {string += " * " + element}; answer*=element;})
-        break;
-    case "-":
-        numbers.forEach(function(element, index) {if (index == 0){string += element; answer = element;} else {string += " - " + element; answer-=element;}})
-        break;
-    case "/":
-        numbers.forEach(function(element, index) {if (index == 0){string += element; answer = element} else {string += " / " + element; answer/=element;}})
-        break;
-    default:
-        console.log("Invalid operator");
+
+function performOneCalculation(){
+    console.log('Please enter an operator:');
+    const operator = readline.prompt();
+    console.log('How many numbers would you like to ' + operator + ':');
+    const quantity = +readline.prompt();
+    
+    function newNumber(i){
+        if (i==0){
+            console.log('Please enter a number:');
+        } else {
+        console.log('Please enter another number:');
+        }
+        let number = +readline.prompt();
+        while (isNaN(number)){
+            console.log('Not a number, please enter a number:');
+            number = +readline.prompt();
+        }
+        numbers.push(number);
+    }
+
+    let numbers = [];
+    for (let i = 0; i < quantity; i++) {
+        newNumber(i)
+    }
+
+    let answer
+    let string = ""
+    switch (operator){
+        case "+":
+            answer = 0;
+            numbers.forEach(function(element, index) {if (index == 0){string += element;} else {string += " + " + element}; answer+=element;})
+            break;
+        case "*":
+            answer = 1;
+            numbers.forEach(function(element, index) {if (index == 0){string += element;} else {string += " * " + element}; answer*=element;})
+            break;
+        case "-":
+            numbers.forEach(function(element, index) {if (index == 0){string += element; answer = element;} else {string += " - " + element; answer-=element;}})
+            break;
+        case "/":
+            numbers.forEach(function(element, index) {if (index == 0){string += element; answer = element} else {string += " / " + element; answer/=element;}})
+            break;
+        default:
+            console.log("Invalid operator");
+    }
+    console.log(string + " is " + answer );
 }
-console.log(string + " is " + answer );
+
+printWelcomeMessage()
+performOneCalculation()
+while (goAgain()){
+    performOneCalculation()
+}
